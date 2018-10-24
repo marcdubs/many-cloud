6982db21607db3388f0b5b0a0cbad84ad1f68198
MDubs <marc50000@gmail.com>
2018-10-24 17:16:51 -0400

    Add credentials provider

--- features/step_definitions/box_steps.js
+++ features/step_definitions/box_steps.js
@@ -1,14 +1,31 @@
-const { Before, Given, When, Then, setDefaultTimeout } = require("cucumber");
+const {
+  BeforeAll,
+  AfterAll,
+  Given,
+  When,
+  Then,
+  setDefaultTimeout
+} = require("cucumber");
 const deep_equal = require("deep-equal");
 const assert = require("assert");
 
 let firstTimeGettingCredentials = true;
 const fs = require("fs");
+const fetch = require("node-fetch");
 
 setDefaultTimeout(30000);
 
-const _getBoxCredentials = function() {
+const _getBoxCredentials = async function() {
   if (process.env.IS_CI && firstTimeGettingCredentials) {
+    let ref_ = await fetch(process.env.CI_CREDS_URI + "get_ref_token", {
+      method: "POST",
+      body: JSON.stringify({
+        t: process.env.CREDS_REQ_TOKEN
+      }),
+      headers: { "Content-Type": "application/json" }
+    });
+    let ref = await ref_.json();
+
     firstTimeGettingCredentials = false;
     const creds = {
       installed: {
@@ -17,7 +34,7 @@ const _getBoxCredentials = function() {
       },
       tokens: {
         access_token: "toget",
-        refresh_token: process.env.CI_BOX_REFRESH_TOKEN
+        refresh_token: ref
       }
     };
     fs.writeFileSync("./credentials/box.json", JSON.stringify(creds), "utf8");
@@ -25,12 +42,30 @@ const _getBoxCredentials = function() {
   return require("../../credentials/box.json");
 };
 
+BeforeAll(async function() {
+  await _getBoxCredentials();
+});
+
+AfterAll(async function() {
+  if (process.env.IS_CI) {
+    let creds = await _getBoxCredentials();
+    await fetch(process.env.CI_CREDS_URI + "set_ref_token", {
+      method: "POST",
+      body: JSON.stringify({
+        t: process.env.CREDS_REQ_TOKEN,
+        new_token: creds.tokens.refresh_token
+      }),
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+});
+
 Given("I create a {string} integration", function(name) {
   this.integration = require("../../").integration(name);
 });
 
 Given("I connect to the demo box account", async function() {
-  let credentials = _getBoxCredentials();
+  let credentials = await _getBoxCredentials();
   let props = JSON.parse(JSON.stringify(credentials.tokens));
   props.credentials = credentials;
   this.connection = await this.integration(props);
