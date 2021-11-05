272f920de8077e4897c1d5a9e5c630230c4a997c
Marcus Dubreuil <marcusdubreuil@gmail.com>
2021-11-05 19:27:14 -0400

    Refactor abstract Item/File/Folder to use classes with clear jsdoc

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
   testURL: "http://localhost",
   bail: true,
   testEnvironment: "node",
+  coveragePathIgnorePatterns: [
+    "<rootDir>/lib/abstractions/file.js",
+    "<rootDir>/lib/abstractions/folder.js",
+    "<rootDir>/lib/abstractions/item.js"
+  ]
 };
